6981e1706edf6e966a0543fcea1dc2ca7ce0a3ff
Chris Stead <cmstead@gmail.com>
2014-09-08 18:42:13 +0000

    Added last, push and instantiation with initial values.

--- src/phlist.php
+++ src/phlist.php
@@ -6,12 +6,31 @@ class PhList{
 
     public function __construct(){
         $this->list = array();
+        $this->initListValues(func_get_args());
+    }
+
+    public function last(){
+        $lastIndex = sizeof($this->list) - 1;
+        return $this->list[$lastIndex];
+    }
+    
+    public function push($value){
+        array_push($this->list, $value);
     }
 
     public function toArray(){
         return $this->list;
     }
 
+    /* Private functions for initialization */
+    
+    private function initListValues($args){
+        if(sizeof($args)){
+            foreach($args as $key=>$argument){
+                array_push($this->list, $argument);
+            }
+        }
+    }
 }
 
 ?>
--- test/phlistTest.php
+++ test/phlistTest.php
@@ -28,6 +28,41 @@ class PhlistTests extends PHPUnit_Framework_TestCase{
 
         $this->assertEquals(gettype($returnedValue), "array");
     }
+    
+    public function testPhListIsInstantiableWithASingleValue(){
+        $testList = new PhList("test value 1");
+        $this->assertEquals(sizeof($testList->toArray()), 1);
+    }
+    
+    public function testPhListIsInstantiableWithMultipleValues(){
+        $testList = new PhList("1", "2", "3");
+        $this->assertEquals(sizeof($testList->toArray()), 3);
+    }
+    
+    public function testLastGetsLastValueOfList(){
+        $testList = new PhList(1, 2, 3, 4);
+        $this->assertEquals($testList->last(), 4);
+    }
+    
+    public function testPushAddsValueToEndOfList(){
+        $testList = new PhList(1, 2);
+        $testList->push(3);
+        
+        $this->assertEquals($testList->last(), 3);
+    }
+    
+    public function testPushThrowsErrorIfNoValueIsProvided(){
+        $testList = new PhList(1, 2);
+        $exceptionThrown = false;
+        
+        try{
+            $testList->push();
+        } catch (Exception $e){
+            $exceptionThrown = true;
+        }
+        
+        $this->assertEquals($exceptionThrown, true);
+    }
 
 }
 
